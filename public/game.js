7a99f6467f1396158558ff86d2a05ccd2d7b6aa5
AbdullrahmanTurk <149819429+AbdullrahmanTurk@users.noreply.github.com>
2024-05-07 12:55:24 +0200

    screens

--- public/game.js
+++ public/game.js
@@ -7,44 +7,61 @@ let lightShots = [];
 let score = 0;
 let lives = 2;
 let level = 1;
+let gameOverFlag = false;
+let gameStarted = false;
+let startButton;
+let stars = [];
 
 function setup() {
     createCanvas(800, 800);
     setInterval(spawnUfo, 3000);
-    createStatScreen(); 
-}
+    createStatScreen();
+    createStartScreen();
+    generateStars(); 
 
 noStroke();
+} 
 
 function draw() {
-    background (0, 10, 30);
-    spaceship();
-    jets();
-    moveLightShots();
-    displayLightShots();
-    
-    for (let i = gameObjects.length - 1; i >= 0; i--) {
-        gameObjects[i].display();
-        gameObjects[i].move(); 
+    if (!gameStarted) {
+        drawStartScreen();
+    } else {
+        background(0, 10, 30);
+        drawStars();
+        spaceship();
+        jets();
+        moveLightShots();
+        displayLightShots();
 
-        if (gameObjects[i].x <= -100) {
-            gameObjects.splice(i, 1);
-        } 
+        if (!gameOverFlag) {
+            for (let i = gameObjects.length - 1; i >= 0; i--) {
+                gameObjects[i].display();
+                gameObjects[i].move();
 
-        for (let j = lightShots.length - 1; j >= 0; j--) {
-            if (dist(gameObjects[i].x, gameObjects[i].y, lightShots[j].x, lightShots[j].y) < 50) {
-                gameObjects.splice(i, 1);
-                lightShots.splice(j, 1);
-                score += 10; // Increase score when UFO is destroyed
-                break; 
-            } 
-        }      
+                if (gameObjects[i].x <= -100) {
+                    gameObjects.splice(i, 1);
+                }
+
+                if (dist(gameObjects[i].x, gameObjects[i].y, x, y) < 100) {
+                    gameOver();
+                }
+
+                for (let j = lightShots.length - 1; j >= 0; j--) {
+                    if (dist(gameObjects[i].x, gameObjects[i].y, lightShots[j].x, lightShots[j].y) < 50) {
+                        gameObjects.splice(i, 1);
+                        lightShots.splice(j, 1);
+                        score += 10;
+                        break;
+                    }
+                }
+            }
+
+            y += spaceshipYSpeed;
+            y = constrain(y, 75, height - 75);
+
+            updateStatScreen();
+        }
     }
-    
-    y += spaceshipYSpeed;
-    y = constrain(y, 75, height - 75);
-    
-    updateStatScreen(); // Update the stat screen
 }
 
 class Ufo {
@@ -54,7 +71,7 @@ class Ufo {
         this.width = 100;
         this.height = 50;
         this.moveSpeed = -5;
-        this.delay = 800; // Delay of 8 seconds
+        this.delay = 800;
         this.timer = 0;
     }
 
@@ -62,21 +79,21 @@ class Ufo {
         fill(200, 0, 0);
         arc(this.x, this.y, 100, 50, PI, PI + PI, PIE);
         fill(0, 200, 255);
-        arc(this.x, this.y-20, 60, 60, PI, PI + PI, PIE);
+        arc(this.x, this.y - 20, 60, 60, PI, PI + PI, PIE);
         fill(255, 255, 0);
-        ellipse(this.x, this.y-10, 10);
-        ellipse(this.x-20, this.y-10, 10);
-        ellipse(this.x-35, this.y-10, 10);
-        ellipse(this.x+20, this.y-10, 10);
-        ellipse(this.x+35, this.y-10, 10);
+        ellipse(this.x, this.y - 10, 10);
+        ellipse(this.x - 20, this.y - 10, 10);
+        ellipse(this.x - 35, this.y - 10, 10);
+        ellipse(this.x + 20, this.y - 10, 10);
+        ellipse(this.x + 35, this.y - 10, 10);
     }
 
     move() {
         this.timer++;
         if (this.timer >= this.delay) {
-            this.x += this.moveSpeed; // Move the UFO to the left
+            this.x += this.moveSpeed; 
         }
-    } 
+    }
 }
 
 function spawnUfo() {
@@ -89,14 +106,14 @@ function spawnUfo() {
 function spaceship() {
     push();
     fill(20, 0, 220);
-    quad(x, y-75, x, y+75, x-75, y+50, x-75, y-50);
-    quad(x, y-75, x, y+75, x+200, y+25, x+200, y-25);
-    rect(x, y-130, 30, 80);
-    rect(x, y+50, 30, 80);
-    rect(x-75, y-150, 250, 50);
-    rect(x-75, y+100, 250, 50);
+    quad(x, y - 75, x, y + 75, x - 75, y + 50, x - 75, y - 50);
+    quad(x, y - 75, x, y + 75, x + 200, y + 25, x + 200, y - 25);
+    rect(x, y - 130, 30, 80);
+    rect(x, y + 50, 30, 80);
+    rect(x - 75, y - 150, 250, 50);
+    rect(x - 75, y + 100, 250, 50);
     fill(0, 200, 255);
-    ellipse(x+100, y, 100, 50);
+    ellipse(x + 100, y, 100, 50);
     pop();
 }
 
@@ -105,31 +122,31 @@ function jets() {
     push();
     fill(255, 255, 0);
     beginShape();
-    vertex(x-80, y+40);
-    bezierVertex(x-80, y+40, x-500, y, x-80, y-40);
+    vertex(x - 80, y + 40);
+    bezierVertex(x - 80, y + 40, x - 500, y, x - 80, y - 40);
     endShape();
     beginShape();
-    vertex(x-80, y-110);
-    bezierVertex(x-80, y-110, x-400, y-125, x-80, y-140);
+    vertex(x - 80, y - 110);
+    bezierVertex(x - 80, y - 110, x - 400, y - 125, x - 80, y - 140);
     endShape();
     beginShape();
-    vertex(x-80, y+110);
-    bezierVertex(x-80, y+110, x-400, y+125, x-80, y+140);
+    vertex(x - 80, y + 110);
+    bezierVertex(x - 80, y + 110, x - 400, y + 125, x - 80, y + 140);
     endShape();
     pop();
     // orange
     fill(255, 180, 0);
     beginShape();
-    vertex(x-80, y+30);
-    bezierVertex(x-80, y+30, x-300, y, x-80, y-30);
+    vertex(x - 80, y + 30);
+    bezierVertex(x - 80, y + 30, x - 300, y, x - 80, y - 30);
     endShape();
     beginShape();
-    vertex(x-80, y-115);
-    bezierVertex(x-80, y-115, x-250, y-125, x-80, y-135);
+    vertex(x - 80, y - 115);
+    bezierVertex(x - 80, y - 115, x - 250, y - 125, x - 80, y - 135);
     endShape();
     beginShape();
-    vertex(x-80, y+115);
-    bezierVertex(x-80, y+115, x-250, y+125, x-80, y+135);
+    vertex(x - 80, y + 115);
+    bezierVertex(x - 80, y + 115, x - 250, y + 125, x - 80, y + 135);
     endShape();
 }
 
@@ -138,7 +155,7 @@ function keyPressed() {
         spaceshipYSpeed -= spaceshipYSpeedIncrement;
     } else if (keyCode === DOWN_ARROW) {
         spaceshipYSpeed += spaceshipYSpeedIncrement;
-    } else if (key === ' ') { 
+    } else if (key === ' ') {
         fireLightShot();
     }
 }
@@ -150,9 +167,9 @@ function keyReleased() {
 }
 
 function fireLightShot() {
-    for (let i = 0; i < 5; i++) { 
+    for (let i = 0; i < 5; i++) {
         let lightShot = {
-            x: x + 200, 
+            x: x + 200,
             y: y + i * 20 - 40,
             size: 10,
             speed: 10
@@ -174,7 +191,7 @@ function displayLightShots() {
     fill(255, 255, 0);
     for (let i = 0; i < lightShots.length; i++) {
         ellipse(lightShots[i].x, lightShots[i].y, lightShots[i].size);
-    } 
+    }
 }
 
 function createStatScreen() {
@@ -191,4 +208,51 @@ function updateStatScreen() {
     let statDiv = select('#statDiv');
     statDiv.html(`Score: ${score}<br>Lives: ${lives}<br>Level: ${level}`);
 }
- 
+
+function gameOver() {
+    gameOverFlag = true; 
+    noLoop();
+    background(0);
+    textAlign(CENTER);
+    textSize(50);
+    fill(255);
+    text("Game Over", width / 3, height / 3); 
+}
+
+function createStartScreen() {
+    startButton = createButton('Start');
+    startButton.position(width / 2 - 50, height / 2);
+    startButton.mousePressed(startGame);
+}
+
+function startGame() {
+    gameStarted = true;
+    startButton.remove(); // Remove the start button
+}
+
+function drawStartScreen() {
+    background(0);
+    textAlign(CENTER);
+    textSize(50);
+    fill(255);
+    text("Space Shooter", width / 2, height / 2 - 50);
+    textSize(20);
+    text("Press Start to begin", width / 2, height / 2 + 50);
+}
+
+function generateStars() {
+    for (let i = 0; i < 200; i++) {
+        stars.push({
+            x: random(width),
+            y: random(height),
+            size: random(1, 3)
+        });
+    }
+}
+
+function drawStars() {
+    fill(255);
+    for (let star of stars) {
+        ellipse(star.x, star.y, star.size);
+    }
+}
